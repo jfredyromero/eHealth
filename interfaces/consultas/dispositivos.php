75eeb92fa1cd3be007ee452816194dbfa2f6545b
Lina Virginia <linavm@unicauca.edu.co>
2021-03-19 13:40:01 -0500

    Actualizados

--- interfaces/consultas/dispositivos.php
+++ interfaces/consultas/dispositivos.php
@@ -77,7 +77,7 @@
                     if (isset($_GET["submit"]) && !empty($_GET["submit"])) {
                         $id = $_GET["id_tarjeta"];
 
-                        $sql5 = "SELECT estado from datos_dispositivos WHERE id= $id";
+                        $sql5 = "SELECT estado from datos_dispositivos WHERE id_tarjeta= $id";
                         $result5 = $mysqli->query($sql5);
                         $row5 = $result5->fetch_array(MYSQLI_NUM);
                         if ($row5 == NULL) {
@@ -92,8 +92,7 @@
                             <input type="number" class="form-control" name="id_tarjeta" placeholder="ID de la tarjeta..." required>
                         </td>
                         <td style="border: none;" valign="top" align=center colspan=2>
-                            <i style="background-color:#281E5D; color:white" class="fas fa-search"></i>
-                            <input style="background-color:#281E5D; color:white"  type="submit" name="submit" value= "Buscar">
+                            <button style="background-color:#281E5D; color:white" value="Buscar" type="submit" class="btn btn-lg" name="submit"><i style="background-color:#281E5D; color:white" class="fas fa-search"></i><span class="pl-3">Buscar</span></button>
                         </td>
                     </form>
                 </tr>
@@ -104,16 +103,16 @@
                         <b>ID de la Tarjeta</b>
                     </td>
                     <td valign="center" align=center bgcolor="#E1E1E1">
+                        <b>Propietario</b>
+                    </td>
+                    <td valign="center" align=center bgcolor="#E1E1E1">
                         <b>Ubicación</b>
                     </td>
                     <td valign="center" align=center bgcolor="#E1E1E1">
                         <b>Estado</b>
                     </td>
                     <td valign="center" align=center bgcolor="#E1E1E1">
-                        <b>Fecha de último registro</b>
-                    </td>
-                    <td valign="center" align=center bgcolor="#E1E1E1" >
-                        <b>Nombre de Tarjeta</b>
+                        <b>Último registro</b>
                     </td>
                     <td valign="center" align=center bgcolor="#E1E1E1" width=80px>
                         <b>Editar</b>
@@ -124,7 +123,7 @@
 
                 $mysqli1 = new mysqli($host, $user, $pw, $db); // Aqu� se hace la conexi�n a la base de datos.
                 // la siguiente linea almacena en una variable denominada sql1, la consulta en lenguaje SQL que quiero realizar a la base de datos. Se consultan los datos de la tarjeta 1, porque en la tabla puede haber datos de diferentes tarjetas.
-                $sql1 = "SELECT * from datos_dispositivos order by id"; // Aqu� se ingresa el valor recibido a la base de datos.
+                $sql1 = "SELECT * from datos_dispositivos order by id_tarjeta"; // Aqu� se ingresa el valor recibido a la base de datos.
                 //
                 // la siguiente l�nea ejecuta la consulta guardada en la variable sql, con ayuda del objeto de conexi�n a la base de datos mysqli
                 $result1 = $mysqli->query($sql1);
@@ -132,11 +131,11 @@
                 // tenga alg�n registro resultante. Como la consulta arroja 5 resultados, los �ltimos que tenga la tabla, se ejecutar� 5 veces el siguiente ciclo while.
                 // el resultado de cada registro de la tabla, se almacena en el arreglo row, row[0] tiene el dato del 1er campo de la tabla, row[1] tiene el dato del 2o campo de la tabla, as� sucesivamente
                 if (isset($_GET["submit"]) && !empty($_GET["submit"])) {
-                    $sql2 = "SELECT MAX(fecha) from datos_medidos WHERE ID_TARJ= $id";
+                    $sql2 = "SELECT MAX(fecha) from datos_medidos WHERE id_tarjeta= $id";
                     $result2 = $mysqli->query($sql2);
                     $row2 = $result2->fetch_array(MYSQLI_NUM);
                     $fecha = $row2[0];
-                    $sql3 = "SELECT estado from datos_dispositivos WHERE id= $id";
+                    $sql3 = "SELECT estado from datos_dispositivos WHERE id_tarjeta= $id";
                     $result3 = $mysqli->query($sql3);
                     $row3 = $result3->fetch_array(MYSQLI_NUM);
                     $estado = $row3[0];
@@ -170,22 +169,17 @@
                                 <img src="/eHealth/static/img/dibujar.png" width=32 height=32>
                             </a>
                         </td>
-                        <td valign="center" align=center>
-                            <a href="#">
-                                <img src="/eHealth/static/img/basura.png" width=32 height=32>
-                                <a>
-                        </td>
-
                     </tr>
                     <?php
                 } else {
                     $contador = 0;
                     while ($row1 = $result1->fetch_array(MYSQLI_NUM)) {
                         $ID_TARJ = $row1[0];
+                        $propietario = $row1[3];
                         $estado = $row1[1];
-                        // $ubi = $row1[2];
+                        $ubicacion = $row1[2];
                         $contador++;
-                        $sql2 = "SELECT MAX(fecha) from datos_medidos WHERE ID_TARJ= $contador";
+                        $sql2 = "SELECT MAX(fecha) from datos_medidos WHERE id_tarjeta= $contador";
                         $result2 = $mysqli->query($sql2);
                         $row2 = $result2->fetch_array(MYSQLI_NUM);
                         $fecha = $row2[0];
@@ -195,7 +189,10 @@
                                 <?php echo $ID_TARJ; ?>
                             </td>
                             <td valign="center" align=center>
-                                <?php echo "1"; ?>
+                                <?php echo $propietario; ?>
+                            </td>
+                            <td valign="center" align=center>
+                                <?php echo $ubicacion; ?>
                             </td>
                             <td valign="top" align=center>
                                 <?php
@@ -206,7 +203,6 @@
                                 } else {
                                 ?>
                                     <img src="/eHealth/static/img/cancelar.png" width=32 height=32>
-
                                 <?php
                                 }
                                 ?>
@@ -217,10 +213,6 @@
                             <td valign="center" align=center>
                                 <input type="image" name="botondeenvio" src="/eHealth/static/img/dibujar.png" width=32 height=32>
                             </td>
-                            <td valign="center" align=center>
-                                <img src="/eHealth/static/img/basura.png" width=32 height=32>
-                            </td>
-
                         </tr>
 
                 <?php
@@ -233,8 +225,8 @@
                 <tr>
                     <td style="border: none;" valign="top" align=center colspan=4>
                     </td>
-                    <td style="border: none;" valign="top" align=center colspan=2>
-                        <a class="btn btn-lg btn-block" style="background-color:#281E5D; color:white" href="/eHealth/interfaces/consultas/añadir.php" role="button">
+                    <td style="border: none;" valign="top" align=right colspan=2>
+                        <a class="btn btn-lg" style="background-color:#281E5D; color:white" href="/eHealth/interfaces/consultas/añadir.php" role="button">
                             <i class="fas fa-plus-circle"></i>
                             <span class="pl-3">Nuevo</span>
                         </a>
@@ -252,4 +244,3 @@
 </body>
 
 </html>
-
